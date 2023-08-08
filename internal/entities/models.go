def3d1848f6e372982b3e3cf39502d503adcac6a
ByteNinja42 <dory01check@gmail.com>
2023-08-08 11:54:41 +0200

    created service structure and method for creating new Expense

--- internal/entities/models.go
+++ internal/entities/models.go
@@ -1,1 +1,19 @@
 package entities
+
+import "time"
+
+type ExpenseRequest struct {
+	Name        string  `json:"name"`
+	Price       float64 `json:"price"`
+	Date        string  `json:"date"`
+	Category    string  `json:"category"`
+	Description string  `json:"description,omitempty"`
+}
+
+type Expense struct {
+	Name        string    `json:"name"`
+	Price       float64   `json:"price"`
+	Date        time.Time `json:"date"`
+	Category    string    `json:"category"`
+	Description string    `json:"description,omitempty"`
+}
